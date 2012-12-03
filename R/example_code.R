9b213212b4a0a4b6dddd4ccb99765525e878ddf4
selbyh <selbyh@bu.edu>
2012-12-03 15:47:19 -0500

    Added some comments to compute_barcode and probability_barcode given I have difficulty understanding the code explicitly every time I go to work on it. Determined the probability for each subtype (ER_HER2,HER2,ER_HER2_HP, and ER_HER2_LP) using subtypes.weights. Have to be very careful using the weighted.means internal as a function will run even if you don't assign it weights:  will include the function in the code to avoid any errors Sorted the probabilities for each subtype in decreasing order, and then took the top 10. Am still working with original dataset -- will try a different one this afternoon.

--- R/example_code.R
+++ R/example_code.R
@@ -6,18 +6,20 @@ rm(list=ls())
 # barcode and likelihood
 
 `compute.barcode` <- function(dataset, adjmatrix)
-{
-	edges <- which(adjmatrix == 1)
+{	
+	edges <- which(adjmatrix == 1) ## a vector of indices
 	barcode <- do.call(rbind, lapply(edges, function(edge, nn) {
-						j <- edge%%nn #modulus
+						j <- edge%%nn ## index 1:100
 						if(j == 0)
-							j <- nn         
-						i <- ceiling(edge/nn)
-						c(i, j,dataset[, i] > dataset[, j])
+							j <- nn   ## if j == 0, replace 0 by 100 
+						
+						i <- ceiling(edge/nn)  ## column index of genes
+						
+						c(i, j,dataset[, i] > dataset[, j])   ## select columns of dataset, j is an element of 1:100 if nn=100
 					}, nn=ncol(adjmatrix)))
-	barcode <- barcode[barcode[ ,1] != barcode[ ,2], , drop=FALSE]
-	rownames(barcode) <- paste(rownames(adjmatrix)[barcode[ ,1]], rownames(adjmatrix)[barcode[ ,2]], sep="_")
-	barcode <- barcode[ ,-c(1,2), drop=FALSE]
+	barcode <- barcode[barcode[ ,1] != barcode[ ,2], , drop=FALSE]  ## removes duplicated rows so gene1_gene2 and gene2_gene1 only have one
+	rownames(barcode) <- paste(rownames(adjmatrix)[barcode[ ,1]], rownames(adjmatrix)[barcode[ ,2]], sep="_")  ## rownames gene1_gene2
+	barcode <- barcode[ ,-c(1,2), drop=FALSE]  
 	return(barcode)
 }
 
@@ -29,7 +31,7 @@ rm(list=ls())
 						if(j == 0)
 							j <- nn        
 						i <- ceiling(edge/nn)
-						c(i, j, weighted.mean(dataset[, i] > dataset[, j], w=weights, na.rm=TRUE))
+						c(i, j, weighted.mean(dataset[, i] > dataset[, j], w=weights, na.rm=TRUE)) #weighted.mean function:  be really careful (will extract function and inlude it in code) because if you don't assign weights it will not crash, so you sometimes don't know what exactly it is doing (at least I did not)
 					}, nn=ncol(adjmatrix)))
 	barcode <- barcode[barcode[ ,1] != barcode[ ,2], , drop=FALSE]
 	rownames(barcode) <- paste(rownames(adjmatrix)[barcode[ ,1]], rownames(adjmatrix)[barcode[ ,2]], sep="_")
@@ -69,7 +71,7 @@ library(jetset)
 # training set
 # let's use EXPO for training our subtype classification model
 # load EXPO dataset
-load(file.path("data","EXPO.RData"))
+load(file.path("../data","EXPO.RData"))
 # use jetset to get one probe per gene
 # jetset results in csv
 jetset_csvs <- c("jetset.scores.hgu95av2_1.2.0.csv", "jetset.scores.hgu133a_1.2.0.csv", "jetset.scores.hgu133plus2_1.2.0.csv", "jetset.scores.u133x3p_1.2.0.csv")
@@ -116,8 +118,33 @@ aa <- matrix(0, ncol=ncol(data), nrow=ncol(data), dimnames=list(colnames(data), 
 # test with on;y the first 100 genes
 aa[1:100, 1:100] <- 1
 
-# barcode
-binary_barcode <- compute.barcode(dataset=data, adjmatrix=aa)
-probability_barcode <- compute.barcode.proba(dataset=data, adjmatrix=aa)
+subtype.weights <- data.frame(subtype.weights, header = TRUE)
+ER_HER2_weights <- subtype.weights$ER..HER2.
+HER2_weights <- subtype.weights$HER2.
+ER_HER2_HP_weights <- subtype.weights$ER..HER2..High.Prolif
+ER_HER2_Low_Prolif_weights <- subtype.weights$ER..HER2..Low.Prolif
 
-#Select a feature set of gene pairs
+
+# binary barcode
+binary_barcode <- compute.barcode(dataset=data, adjmatrix=aa)
+
+# probability barcode for each subtype: ER_HER2, HER2, ER_HER2_HP, ER_HER2_LP:
+ER_HER2 <- compute.barcode.proba(dataset=data, weights = ER_HER2_weights, adjmatrix=aa)
+HER2 <- compute.barcode.proba(dataset=data, weights = HER2_weights, adjmatrix=aa)
+ER_HER2_HP <- compute.barcode.proba(dataset=data, weights = ER_HER2_HP_weights, adjmatrix=aa)  ## What is High_Profile and Low_Profile?
+ER_HER2_LP <- compute.barcode.proba(dataset=data, weights = ER_HER2_LP_weights, adjmatrix=aa)
+
+# sort decreasing order
+ER_HER2_sorted <- ER_HER2[order(ER_HER2[,3],decreasing = T),]	## decreasing order here
+HER2_sorted <- HER2[order(HER2[,3]),decreasing = T]
+ER_HER2_HP_sorted <- HER2[order(ER_HER2_HP[,3]),decreasing = T]
+ER_HER2_LP_sorted <- HER2[order(ER_HER2_LP[,3]),decreasing = T]
+
+# select top 10 gene pairs for each subtype
+ER_HER2_Top10 <- ER_HER2_sorted[1:10,]
+HER2_Top10 <- HER2_sorted[1:10,]
+ER_HER2_HP_Top10 <- ER_HER2_sorted[1:10,]
+ER_HER2_LP_Top10 <- ER_HER2_sorted[1:10,]
+
+
+# ToDo: Will write a function to determine probability of given subtype, sort, and take top 10.  This is too specific to dataset.
